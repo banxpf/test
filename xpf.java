6c1231d045ce8efffeadd4fd8e8f47522f57a958
徐鹏飞 <xupengfei@wanmi.com>
2023-02-17 15:48:02 +0800

    测试提交

---
+++ xpf.java
@@ -0,0 +1,13 @@
+/**
+ * @className xpf
+ * @description TODO
+ * @author xpf
+ * @date 2023/2/17 3:47 PM
+ **/
+public class xpf {
+    public static void main(String[] args) {
+
+
+        System.out.println("dsdsds ");
+    }
+}
